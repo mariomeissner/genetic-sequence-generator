ab1f75db687d46005b92d870e682ba13cb336ab6
mariomeissner <mr.rash.boy@gmail.com>
2017-02-19 19:54:24 +0100

    Documentation and public/private

--- sequence.js
+++ sequence.js
@@ -1,5 +1,6 @@
+"use strict";
 /**
- * Sequence of 7 blocks of 4 bits. They represent a genetic string. They can be decoded 
+ * Sequence of 7 blocks of 4 bits. They represent a genetic string. They can be decoded
  * to a sequence of numbers and operators, each block being one number or operator.
  * Blocks with value below 1010 represent their corresponding integer (ex: 0101 = 5)
  * Blocks with value 1010 or above represent an operator:
@@ -8,124 +9,118 @@
  * 1100 = *
  * 1101 = /
  * The remaining possible blocks will be ignored.
- * 
+ *
  */
-export default class Sequence {
-    public originalSequence : string //Should remain untouched.
-    private sequence : string //Actual string we will work with.
-    length : number; //May be modified by clean.
-
-    constructor(sequence : string) {
+class Sequence {
+    constructor(sequence) {
         this.originalSequence = sequence;
         this.sequence = sequence;
         this.length = this.originalSequence.length / 4;
-        if (!Number.isInteger(this.length)){
-            console.log("ERROR, Bad sequence length")
+        if (!Number.isInteger(this.length)) {
+            console.log("ERROR, Bad sequence length");
         }
         this.clean();
     }
-
     /**
-     * Looks for nonsense such as two consecutive operators, consecutive numbers or blocks without meaning. 
+     * Looks for nonsense such as two consecutive operators, consecutive numbers or blocks without meaning.
      * Blocks flagged as nonsense will be deleted leaving a shorter sequence.
      * This should be called before working with the sequence.
      */
-    private clean(){
-        let shouldBeNumber : boolean = true; //next block should be a number or not
-        let iterator : number = 0;
-        let current : number;
-        let correct : boolean;
+    clean() {
+        let shouldBeNumber = true; //next block should be a number or not
+        let iterator = 0;
+        let current;
+        let correct;
         while (iterator < this.length) {
             current = this.getBlockValue(iterator);
             //This variable will only resolve to true if the current block is correct
-            correct = (shouldBeNumber && current<=9) || (!shouldBeNumber && current >= 10 && current <=13);
-            if (!correct){ //Not correct. We delete block. 
+            correct = (shouldBeNumber && current <= 9) || (!shouldBeNumber && current >= 10 && current <= 13);
+            if (!correct) {
                 this.deleteBlock(iterator);
                 //We should not update shouldBeNumber or iterator since a new block has been shifted to current position. 
                 continue;
-            } else { //Current block seems to be ok so we advance to next block;
+            }
+            else {
                 shouldBeNumber = !shouldBeNumber;
                 iterator++;
             }
         }
         //Now we need to check whether the last block is an operator, and delete it too.
-        if (shouldBeNumber){ //Means last one was operator
-            this.deleteBlock(this.length-1);
-        } 
+        if (shouldBeNumber) {
+            this.deleteBlock(this.length - 1);
+        }
     }
-
     /**
      * Deletes a block of 4 bits of the sequence at indicated position, shifting the rest to the left.
      */
-    private deleteBlock(position : number) : void {
+    deleteBlock(position) {
         /**
          * We will create two substrings: The string until the block to be deleted,
          * and the string from that block onwards (not included, obviously).
          */
-        let firstPart : string = this.sequence.substr(0, position*4);
-        let lastPart : string = this.sequence.substr((position+1)*4, this.length*4-1);
+        let firstPart = this.sequence.substr(0, position * 4);
+        let lastPart = this.sequence.substr((position + 1) * 4, this.length * 4 - 1);
         this.sequence = firstPart + lastPart; //Overriding sequence
         this.length--;
     }
-
     /**
      * POSSIBLY UNNECESSARY SINCE ORIGINAL BITCODE NOW PUBLIC
      * DO WE NEED TO SHOW CLEANED BITCODE?
      * Returns bitcode sequence string as is.
      */
-    public getBitcode() : string{
+    getBitcode() {
         return this.sequence;
     }
-
     /**
      * Returns the actual number or operator represented by a block of genetic code
      * A number above 9 indicates an operator.
      * 10 = +; 11 = *; 12 = *; 13 = /
      */
-    public getBlockValue(position : number) : number {
-        let block : string = this.sequence.substr(position*4, 4);
+    getBlockValue(position) {
+        let block = this.sequence.substr(position * 4, 4);
         return parseInt(block, 2);
     }
-
     /**
      * Returns the decrypted string corresponding to the cleaned bitcode.
      */
-    public decode(){
-        let iterator : number = 0;
-        let string : string = "";
-        let current : number;
-        while (iterator < this.length){
+    decode() {
+        let iterator = 0;
+        let string = "";
+        let current;
+        while (iterator < this.length) {
             current = this.getBlockValue(iterator);
-            if (current <= 9){
+            if (current <= 9) {
                 string += current;
-            } else {
+            }
+            else {
                 switch (current) {
                     case 10:
-                        string+=" + ";
+                        string += " + ";
                         break;
                     case 11:
-                        string+=" - ";
+                        string += " - ";
                         break;
                     case 12:
-                        string+=" * ";
+                        string += " * ";
                         break;
                     case 13:
-                        string+=" / ";
+                        string += " / ";
                         break;
                     default:
-                        string+=" NaN ";
+                        string += " NaN ";
                 }
             }
             iterator++;
         }
         return string;
     }
-
     /**
      * Returns the outcome value of the sequence according to its representation
-     * Uses JS eval function, which is why we needed to clean the 
+     * Uses JS eval function, which is why we needed to clean the
      */
-    public evaluate() : number {
+    evaluate() {
         return eval(this.decode());
     }
 }
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = Sequence;
