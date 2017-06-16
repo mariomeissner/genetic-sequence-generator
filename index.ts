cacaca9c6c2179e81078fb7735ad4007fa3c8cfa
mariomeissner <mr.rash.boy@gmail.com>
2017-06-16 21:42:52 +0200

    Used for testing.

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
-import Sequence from "./sequence.js";
-import Genetics from "./genetics.js"
-let genetics : Genetics = new Genetics(40, 7*4, 400);
-genetics.evaluate();
-genetics.print();
+import Sequence from "./sequence";
+let sequence : Sequence = new Sequence("0000000000000000000000000000");
+let sequence2 : Sequence = new Sequence("1111111111111111111111111111");
+console.log(sequence.evaluate());
+console.log(sequence.mutate());
+console.log(sequence2.mutate());
