4bbb0519abcdbfbdb046282297508f777e53eb16
mariomeissner <mr.rash.boy@gmail.com>
2017-06-16 21:42:02 +0200

    Relocated compiled JS files into bin.

--- index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-function runTest(sequence) {
-    console.log("Original Bitcode: " + sequence.getBitcode() + " ==> " + sequence.decode());
-    console.log("Cleaned Bitcode: " + sequence.getBitcode() + " ==> " + sequence.decode());
-    console.log("Result of evaluation: " + sequence.evaluate());
-}
